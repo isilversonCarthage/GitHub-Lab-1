52cf17891686d1a02f871369beac5a308059bfff
isilversonCarthage <isilverson@carthage.edu>
2025-03-04 14:03:53 -0600

    adding year calculator and function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 using namespace std;
 
+void printName();
+
 int main()
 {
+    int currentYear = 0;
+    int birthYear = 0;
     cout<<"GitHub Demo"<<endl;
+    void printName();
+    cout<<"enter the current year"<<endl;
+    cin>>currentYear;
+    cout<<"now enter your birth year"<<endl;
+    cin>>birthYear;
+    cout<<"Isaac, you are "<<currentYear - birthYear<<" years old"<<endl;
+
     return 0;
+}
+
+void printName()
+{
+    cout<<"Isaac Silverson"<<endl;
 }
